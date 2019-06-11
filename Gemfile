1b38ffceedbc1bd833c3a75aab6edb5f057ef1b1
tiffanynuugen <tiffanynuugen@gmail.com>
2019-06-10 21:15:39 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,11 +1,10 @@
 source 'https://rubygems.org'
 
 gem 'active_model_serializers', '0.10.6'
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.5.1'
+gem 'rails'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3.13'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,63 @@
+window.onload = () => {
+  attachListeners()
+}
+
+function attachListeners() {
+// listens to when a square is clicked
+  const squares = document.querySelectorAll('td').forEach(square => {
+    square.addEventListener("click", () => {
+        doTurn(square)
+    })
+  })
+}
+
+// global variable
+var turn = 0
+
+// returns "X" or "O" based on either even/odd turn count
+function player() {
+  if (turn % 2 === 0 || isNaN(turn)) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+// adds "X" or "O" on to clicked square
+function updateState(square) {
+  square.innerHTML = player()
+}
+
+function setMessage() {
+  const string = "Player X Won!" || "Player O Won!"
+  const messageDiv = document.getElementById('message').innerHTML = string
+}
+
+// determine the exact position and then get the token of each square
+// if player gets either token in three consecutive squares horizontally,
+// vertically, or diagonally, then player wins.
+function checkWinner() {
+  // const body = document.querySelector("tbody").addEventListener("click", () => {
+  //   const x = event.target.dataset.x
+  //   const y = event.target.dataset.y
+  //   const square = document.querySelector(`[data-x="${x}"][data-y="${y}"]`)
+  // })
+}
+
+function doTurn(square) {
+  updateState(square)
+  turn++
+  checkWinner()
+  setMessage()
+}
+
+
+
+
+
+
+
+
+
+
 // Code your JavaScript / jQuery solution here
