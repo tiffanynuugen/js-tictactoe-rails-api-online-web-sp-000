851745e1a7654955c95ebf4fa8e2ce020f531907
tiffanynuugen <tiffanynuugen@gmail.com>
2019-07-03 00:20:19 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -25,44 +25,91 @@ var turn = 0
 function player() {
   if (turn % 2 === 0 || isNaN(turn)) {
     return "X"
-  } else {
-    return "O"
   }
+  return "O"
 }
 
-// adds "X" or "O" on to clicked square
+function setMessage(string) {
+  const messageDiv = document.getElementById('message').innerHTML = string
+}
+
+// place "X" or "O" on to clicked square
+// BUT cannot place token in a square that is already taken
 function updateState(square) {
-  square.innerHTML = player()
+  // if square is empty, then place "X" or "O" on clicked square.
+  if (square.innerHTML === "") {
+    square.innerHTML = player()
+  } else {
+    // if square is NOT empty, then DO NOT allow user to place a token.
+    // square.innerHTML != player()
+    turn = 0
+  }
+  // debugger
 }
 
 function checkWinner() {
   // create empty array
   const board = []
-  // come up with an array of all possible winning combos - horizontally, vertically, and diagonally
+  // come up with an array of all possible horizontal, vertical, and diagonal winning combos
   const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-  // then iterate over array and get each combo
+  // get all of the X's, O's, or blanks from each of the squares
+  const squares = document.querySelectorAll("td").forEach(square => {
+    board.push(square.innerHTML)
+  })
 
-  // get all of the X's, O's, or blanks from all of the squares
-  const squares = document.querySelectorAll("td").forEach(square => board.push(square.innerHTML))
+  // console.log(board);
+  // return board
+  // debugger
 
-  // winningCombos.forEach(combo => {
-  //   // console.log(combo[0]);
-  //   // console.log(combo[1]);
-  // })
+  // iterate over array and get each combo
+  // then compare three tokens of each square with each other
+  // to see if they match up
 
-  const indicesOfO = board.reduce((acc, el, i) => (el === "O" ? [...acc, i] : acc), [])
-  const indicesOfX = board.reduce((acc, el, i) => (el === "X" ? [...acc, i] : acc), [])
-  // // debugger
-  if (indicesOfX === winningCombos[0] || indicesOfX === winningCombos[1] || indicesOfX === winningCombos[2] || indicesOfX === winningCombos[3] || indicesOfX === winningCombos[4] || indicesOfX === winningCombos[5] || indicesOfX === winningCombos[6] || indicesOfX === winningCombos[7]) {
-    return true
-  } else if (indicesOfO === winningCombos[0] || indicesOfO === winningCombos[1] || indicesOfO === winningCombos[2] || indicesOfO === winningCombos[3] || indicesOfO === winningCombos[4] || indicesOfO === winningCombos[5] || indicesOfO === winningCombos[6] || indicesOfO === winningCombos[7]) {
-    return true
-  } else {
-    return false
-  }
+  // check if tokens of winning combo is either X or O
+  // if three X's or O's, then set either message: "Player X Won!" or "Player O Won!"
 
+  // traditional for loop
+  // * returning false OUTSIDE the loop allows ALL arrays to be checked against
+  // the condition before it checks for no winning combos—compared to when false
+  // was being returned within the loop and ONLY checking the first array against the condition
+  // and skipping all the other arrays
 
+  for (let i = 0; i < winningCombos.length; i++) {
+      combo = winningCombos[i]
+      if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+          if (board[combo[0]] === "X") {
+              setMessage("Player X Won!")
+            } else if (board[combo[0]] === "O") {
+                setMessage("Player O Won!")
+              }
+              return true
+            }
+          }
+          return false
 
+  // some() method checks if elements in array pass condition and returns boolean value
+  // return winningCombos.some(combo => {
+    //    return (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]])
+    // })
+
+  // for...of loop
+  // for (let combo of winningCombos) {
+  //     if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+  //         return true;
+  //       }
+  //     }
+  //     return false;
+
+  // const indicesOfO = board.reduce((acc, el, i) => (el === "O" ? [...acc, i] : acc), [])
+  // const indicesOfX = board.reduce((acc, el, i) => (el === "X" ? [...acc, i] : acc), [])
+
+  // if (indicesOfX.join() === winningCombos[0].join() || indicesOfX.join() === winningCombos[1].join() || indicesOfX.join() === winningCombos[2].join() || indicesOfX.join() === winningCombos[3].join() || indicesOfX.join() === winningCombos[4].join() || indicesOfX.join() === winningCombos[5].join() || indicesOfX.join() === winningCombos[6].join() || indicesOfX.join() === winningCombos[7].join()) {
+  //   return true
+  // } else if (indicesOfO.join() === winningCombos[0].join() || indicesOfO.join() === winningCombos[1].join() || indicesOfO.join() === winningCombos[2].join() || indicesOfO.join() === winningCombos[3].join() || indicesOfO.join() === winningCombos[4].join() || indicesOfO.join() === winningCombos[5].join() || indicesOfO.join() === winningCombos[6].join() || indicesOfO.join() === winningCombos[7].join()) {
+  //   return true
+  // } else {
+  //   return false
+  // }
 
   // and then push them into the empty array
   // get all of the indices of the token
@@ -72,17 +119,32 @@ function checkWinner() {
   // acc = 1, el = "X", i = 2 => [0, 1, 2]
 }
 
-
-function setMessage(string) {
-  const messageDiv = document.getElementById('message').innerHTML = string
-}
-
 function doTurn(square) {
+  // place token on to clicked square
   updateState(square)
+  // then increment by 1
   turn++
-  checkWinner()
-  setMessage()
+  // debugger
+  // if game is won, reset board and turn counter
+  // iterate through squares and clear all tokens
+  if (checkWinner()) {
+    const squares = document.querySelectorAll("td").forEach(square => {
+      square.innerHTML = ""
+    })
+    turn = 0
+  } else if (turn === 9) {
+    // debugger
+    setMessage("Tie game.")
+    const squares = document.querySelectorAll("td").forEach(square => {
+      square.innerHTML = ""
+    })
+    // debugger
+    turn = 0
+  }
 }
+
+
+
 
 
 
--- test/tictactoeTest.js
+++ test/tictactoeTest.js
@@ -114,33 +114,33 @@ describe('tictactoe.js', () => {
 
     it('returns true when a player wins horizontally', () => {
       populateBoard(['X', 'X', 'X', '', '', '', 'O', 'O', '']);
-      //  X | X | X 
+      //  X | X | X
       // -----------
-      //    |   |   
+      //    |   |
       // -----------
-      //  O | O |   
+      //  O | O |
 
       expect(window.checkWinner()).to.be.true;
     });
 
     it('returns true when a player wins diagonally', () => {
       populateBoard(['X', 'X', 'O', '', 'O', '', 'O', 'X', '']);
-      //  X | X | O 
+      //  X | X | O
       // -----------
-      //    | O |   
+      //    | O |
       // -----------
-      //  O | X |   
+      //  O | X |
 
       expect(window.checkWinner()).to.be.true;
     });
 
     it('returns true when a player wins vertically', () => {
       populateBoard(['O', '', 'X', '', 'O', 'X', 'O', '', 'X']);
-      //  O |   | X 
+      //  O |   | X
       // -----------
-      //    | O | X 
+      //    | O | X
       // -----------
-      //  O |   | X 
+      //  O |   | X
 
       expect(window.checkWinner()).to.be.true;
     });
@@ -149,11 +149,11 @@ describe('tictactoe.js', () => {
       expect(window.checkWinner()).to.equal(false);
 
       populateBoard(['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']);
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //  O | X | O 
+      //  O | X | O
 
       expect(window.checkWinner()).to.equal(false);
     });
@@ -162,11 +162,11 @@ describe('tictactoe.js', () => {
       const spy = sandbox.stub(window, 'setMessage');
 
       populateBoard(['', '', '', 'X', 'X', 'X', 'O', 'O', '']);
-      //    |   |   
+      //    |   |
       // -----------
-      //  X | X | X 
+      //  X | X | X
       // -----------
-      //  O | O |   
+      //  O | O |
 
       window.checkWinner();
 
@@ -177,11 +177,11 @@ describe('tictactoe.js', () => {
       const spy = sandbox.stub(window, 'setMessage');
 
       populateBoard(['O', '', '', 'X', 'O', 'X', 'X', '', 'O']);
-      //  O |   |   
+      //  O |   |
       // -----------
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //  X |   | O 
+      //  X |   | O
 
       window.checkWinner();
 
@@ -229,11 +229,11 @@ describe('tictactoe.js', () => {
       const spy = sandbox.spy(window, 'setMessage');
 
       populateBoard(['X', 'O', 'X', 'X', 'O', 'X', 'O', '', 'O']);
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //  O |   | O 
+      //  O |   | O
 
       window.turn = 8;
       window.doTurn(squares[7]);
@@ -245,11 +245,11 @@ describe('tictactoe.js', () => {
       sinon.useFakeXMLHttpRequest();
 
       populateBoard(['X', 'X', 'O', 'X', 'O', 'X', '', 'O', 'O']);
-      //  X | X | O 
+      //  X | X | O
       // -----------
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //    | O | O 
+      //    | O | O
 
       window.turn = 8;
       window.doTurn(squares[6]);
@@ -305,7 +305,7 @@ describe('Gameplay', () => {
     window.turn = 1;
 
     squares[0].click();
-
+    // debugger
     expect(squares[0].innerHTML).to.equal('X');
     expect(window.turn).to.equal(1);
   });
@@ -313,11 +313,11 @@ describe('Gameplay', () => {
   it('Users cannot play any turns once a game is won or tied', () => {
     populateBoard(['X', 'X', 'X', '', '', '', 'O', 'O', '']);
     window.turn = 5;
-    //  X | X | X 
+    //  X | X | X
     // -----------
-    //    |   |   
+    //    |   |
     // -----------
-    //  O | O |   
+    //  O | O |
 
     squares[4].click();
 
@@ -329,11 +329,11 @@ describe('Gameplay', () => {
     sinon.useFakeXMLHttpRequest();
 
     populateBoard(['X', 'O', 'X', 'X', 'O', 'X', 'O', '', 'O']);
-    //  X | O | X 
+    //  X | O | X
     // -----------
-    //  X | O | X 
+    //  X | O | X
     // -----------
-    //  O |   | O 
+    //  O |   | O
 
     window.turn = 8;
     window.doTurn(squares[7]);
@@ -578,11 +578,11 @@ describe('AJAX interactions with the Rails API', () => {
 
     it('auto-saves tie games', () => {
       populateBoard(['X', 'O', 'X', 'X', 'O', 'X', 'O', '', 'O']);
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //  X | O | X 
+      //  X | O | X
       // -----------
-      //  O |   | O 
+      //  O |   | O
 
       window.turn = 8;
       window.doTurn(squares[7]);
@@ -594,11 +594,11 @@ describe('AJAX interactions with the Rails API', () => {
     it('auto-saves won games', () => {
       populateBoard(['X', 'X', '', '', '', '', 'O', 'O', '']);
       window.turn = 4;
-      //  X | X |   
+      //  X | X |
       // -----------
-      //    |   |   
+      //    |   |
       // -----------
-      //  O | O |   
+      //  O | O |
 
       squares[2].click();
 
